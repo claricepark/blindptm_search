235f9fb469904e1756842528e8197c5816d97521
claricep <clariceparky24@gmail.com>
2022-06-27 15:53:42 -0700

    greyed out

--- src/dta/ReadFasta.java
+++ src/dta/ReadFasta.java
@@ -6,13 +6,19 @@ import java.io.BufferedReader;
 
 public class ReadFasta {
 
-    //String inputFile = "/Users/claricepark/data/blindptm/DTASelect-filter.txt";
+    String inputFile = "/Users/claricepark/data/blindptm/identified.fasta";
     //read fasta file.
     //ecah potein, create FastaProtein object and add to List (ArrayList)
 
-    List<FastaProtein> proteinList = new ArrayList();
+    List<ReadFasta> proteinList = new ArrayList();
 
-
+    String eachLine = null;
+    BufferedReader br = new BufferedReader(new FileReader(inputFile));
+    while ((eachLine = br.readLine()) != null){
+        ReadFasta seq = new ReadFasta();
+        proteinList.add(seq);
+    }
+    //System.out.println(proteinList);
 
 
 }
